8aa09da07470da568b0b21511ff616bf1941755c
trushchinskii <trushchinskii@surfstudio.ru>
2019-08-05 11:42:55 +0300

    ANDDEP-645 Подготовка EasyAdapter для новой системы android-standard

--- easyadapter_sample/src/main/java/ru/surfstudio/android/easyadapter_sample/domain/Carousel.kt
+++
@@ -1,8 +0,0 @@
-package ru.surfstudio.android.easyadapter_sample.domain
-
-
-data class Carousel(
-        val id: String,
-        val name: String,
-        val elements: List<Element> = emptyList()
-)
