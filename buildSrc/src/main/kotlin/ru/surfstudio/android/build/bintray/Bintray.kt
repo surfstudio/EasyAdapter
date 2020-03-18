51b467feee01e6e902dd619aec78f971c41e657d
Маргарита Володина <volodina@surfstudio.ru>
2020-03-18 11:20:59 +0000

    Merged in ANDDEP-970-build-system-refactoring (pull request #802) (4fb062c9)

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/bintray/Bintray.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/bintray/Bintray.kt
@@ -1,8 +1,7 @@
 package ru.surfstudio.android.build.bintray
 
 import ru.surfstudio.android.build.Components
-import ru.surfstudio.android.build.exceptions.ArtifactNotExistInBintrayException
-import ru.surfstudio.android.build.model.BintrayRepoLatestVersion
+import ru.surfstudio.android.build.exceptions.bintray.ArtifactNotExistInBintrayException
 import ru.surfstudio.android.build.model.Component
 import ru.surfstudio.android.build.model.module.Library
 
@@ -14,14 +13,14 @@ object Bintray {
     private val repository = BintrayRepository()
 
     /**
-     * Check if libraries's android standard dependencies exist in bintray
+     * Check libraries's android standard dependencies exist in bintray
      */
     fun checkLibrariesStandardDependenciesExisting(component: Component) {
         component.libraries.forEach { this.checkLibraryStandardDependenciesExisting(it, component) }
     }
 
     /**
-     * Check if library's android standard dependencies exist in bintray
+     * Check library's android standard dependencies exist in bintray
      */
     private fun checkLibraryStandardDependenciesExisting(library: Library, component: Component) {
         library.androidStandardDependencies
@@ -42,14 +41,9 @@ object Bintray {
     }
 
     /**
-     * Check if artifact exists in bintray
+     * Check artifact exist in bintray
      */
-    fun isArtifactExists(dependencyName: String, version: String): Boolean =
-            repository.isArtifactVersionExist(dependencyName, version)
-
-    fun getAllPackages(): List<String> =
-            repository.getAllPackages()
-
-    fun getArtifactLatestVersion(artifactName: String): BintrayRepoLatestVersion =
-            repository.getArtifactLatestVersion(artifactName)
+    fun isArtifactExists(dependencyName: String, version: String): Boolean {
+        return repository.isArtifactVersionExist(dependencyName, version)
+    }
 }
