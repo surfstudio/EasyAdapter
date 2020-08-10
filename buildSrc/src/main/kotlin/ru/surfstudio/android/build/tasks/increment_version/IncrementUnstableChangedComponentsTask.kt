07180d96e7ca03ff012c11b9b3660dced3b7289d
ryabkov-surf <ryabkov@surfstudio.ru>
2020-08-10 11:54:44 +0300

    ANDDEP-734 исправлено повторное отправление в чат сообщение о статусе билда; бейдж теперь отображает реальный статус + его ссылка в README ведет на последний билд (c7184ba3)

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/increment_version/IncrementUnstableChangedComponentsTask.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/increment_version/IncrementUnstableChangedComponentsTask.kt
@@ -30,7 +30,7 @@ open class IncrementUnstableChangedComponentsTask : DefaultTask() {
         val currentRevision = GitCommandRunner().getCurrentRevisionShort()
 
         val resultByFiles = ComponentsFilesChecker(currentRevision, revisionToCompare)
-                .getChangeInformationForComponents(ignoreNotLibFiles = true)
+                .getChangeInformationForComponents(ignoreReleaseNotesChanges = true)
 
         val resultsByConfiguration = ComponentsConfigurationChecker(currentRevision, revisionToCompare)
                 .getChangeInformationForComponents()
